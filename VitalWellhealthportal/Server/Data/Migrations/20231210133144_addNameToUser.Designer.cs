bde3953b9bfc2f972808b6887884d6be26abff74
CHAI RYAN <2205145B@student.tp.edu.sg>
2023-12-10 22:31:48 +0800

    Added identity server (Ryan Chai)

--- VitalWellhealthportal/Server/Data/Migrations/20231210133144_addNameToUser.Designer.cs
+++ VitalWellhealthportal/Server/Data/Migrations/20231210133144_addNameToUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VitalWellhealthportal.Server.Data;
 
@@ -11,9 +12,11 @@ using VitalWellhealthportal.Server.Data;
 namespace VitalWellhealthportal.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231210133144_addNameToUser")]
+    partial class addNameToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
